67f628136a3508ccaed4dd6af13c09cf450faaf4
sweetyue9045 <47917697+sweetyue9045@users.noreply.github.com>
2023-10-05 00:42:46 +0800

    update

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -23,6 +23,18 @@ const Board = () => {
     );
 }
 
+const Title = ({ Title_top, Title_bottom, Id, ls, lss }) => {
+    const style = {};
+    if (document.body.clientWidth <= 834) style.letterSpacing = lss;
+    else style.letterSpacing = ls;
+
+    return (
+        <div className="title" id={Id}>
+            <div className="title_top">{Title_top}</div>
+            <div className="title_bottom" style={style}>{Title_bottom}</div>
+        </div>
+    );
+}
 
 export default Board;
 
